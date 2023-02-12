19a5300eede572e7049d496b8d18856df7434e35
reloadercf <elcfm1@gmail.com>
2023-02-12 00:45:24 -0600

    feat: :tada: Init menu navigation and routing

---
+++ src/components/helpers/MainLayoutHelper.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+export const kindsMenu = (selectedKind) => {
+  const kind = ['All', 'travel', 'Lifestyle', 'bussiness'];
+  return kind[selectedKind];
+};
